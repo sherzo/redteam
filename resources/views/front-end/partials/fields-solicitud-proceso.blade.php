2bb3bb3334a81fa8de9d8da768f3a28a729607aa
Saul Florez <saulflorez.backend@gmail.com>
2018-06-11 06:14:17 -0400

    Hotfix link to process

--- resources/views/front-end/partials/fields-solicitud-proceso.blade.php
+++ resources/views/front-end/partials/fields-solicitud-proceso.blade.php
@@ -1,6 +1,6 @@
 
 <div class="col-xs-12 col-sm-12 col-md-12 ProcessSolicitud">
-    <a href="{{ url('process') }}">
+    <a href="{{ url('requests-process') }}">
         <p>Solicitud en proceso (2)</p>
     </a>
 </div>
