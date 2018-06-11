38af7ede3c94a5f4391881f8193052a4a6ba8fd5
Saul Florez <saulflorez.backend@gmail.com>
2018-06-11 06:13:06 -0400

    Link to process

--- resources/views/front-end/partials/fields-solicitud-proceso.blade.php
+++ resources/views/front-end/partials/fields-solicitud-proceso.blade.php
@@ -1,6 +1,6 @@
 
 <div class="col-xs-12 col-sm-12 col-md-12 ProcessSolicitud">
-    <a href="solicitudes-proceso">
+    <a href="{{ url('process') }}">
         <p>Solicitud en proceso (2)</p>
     </a>
 </div>
