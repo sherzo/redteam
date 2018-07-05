0523ae21f82b37e780077439452bc1aa574c99db
Saul Florez <saulflorez.backend@gmail.com>
2018-07-04 20:48:32 -0400

    BD sockert

--- public/app-node/config.js
+++ public/app-node/config.js
@@ -4,7 +4,7 @@ module.exports = {
   port: process.env.PORT || 6800,
   host: 'localhost',
   user: 'root',
-  password: 'root',
-  database: 'e216*',
+  password: 'e216*',
+  database: 'intranet',
   port_db: 3306
 }
